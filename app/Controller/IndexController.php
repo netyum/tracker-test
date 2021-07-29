9f657822c9283d7d0df89d57208d3951529c1d25
wayhood <wayhood@wayhooddeMacBook-Pro.local>
2021-07-29 11:54:40 +0800

    add redis

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -12,6 +12,10 @@ declare(strict_types=1);
 namespace App\Controller;
 use Hyperf\Guzzle\ClientFactory;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\Utils\Coroutine;
+use Hyperf\Utils\Exception\ParallelExecutionException;
+use Hyperf\Utils\Parallel;
+use Hyperf\Redis\Redis;
 
 class IndexController extends AbstractController
 {
@@ -21,16 +25,24 @@ class IndexController extends AbstractController
      */
     public $clientFactory;
 
+    /**
+     * @Inject
+     * @var Redis
+     */
+    public $redis;
+
     public function index()
     {
-        $client = $this->clientFactory->create([]);
-        $response = $client->request("GET", 'http://www.baidu.com');
-        $body = $response->getBody();
-        $data = "";
-        while (!$body->eof()) {
-            $data .= $body->read(1024);
-        }
-        //var_dump($data);
+        // http
+        $this->getData("http://www.baidu.com");
+        $this->coGet();
+
+        // redis
+        $this->redis->set("TEST", '11111', 100);
+        echo $this->redis->get("TEST"), "\n";
+        $this->coRedis();
+        $this->coGetRedis();
+
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
 
@@ -38,5 +50,84 @@ class IndexController extends AbstractController
             'method' => $method,
             'message' => "Hello {$user}.",
         ];
+    }
+
+    public function coGetRedis() {
+        $keys = ['a', 'b', 'c', 'd'];
+
+        $wg = new \Hyperf\Utils\WaitGroup();
+        foreach($keys as $key) {
+            $wg->add(1);
+            Coroutine::create(function () use ($wg, $key) {
+                Coroutine::defer(function() use($wg) {
+                    $wg->done();
+                });
+                $data = $this->redis->get($key);
+                echo $key, " -------- ", $data, "\n";
+            });
+        }
+        $wg->wait();
+    }
+
+    public function coRedis() {
+        $keys = ['a', 'b', 'c', 'd'];
+
+        $parallel = new Parallel(count($keys));
+        foreach ($keys as $key) {
+            $parallel->add(function () use ($key) {
+                $ret = $this->redis->set($key, $key, 100);
+                return [$key, $ret, Coroutine::id()];
+            });
+        }
+
+        try{
+            $results = $parallel->wait();
+            foreach($results as $v) {
+                echo $v[0], '--------', $v[1], " -------- ", $v[2], "\n";
+            }
+        } catch(ParallelExecutionException $e){
+            var_dump($e->getMessage());
+        }
+    }
+
+    public function coGet() {
+
+        $urls = [
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+            'http://www.baidu.com',
+        ];
+
+        $wg = new \Hyperf\Utils\WaitGroup();
+        foreach($urls as $url) {
+            $wg->add(1);
+            Coroutine::create(function () use ($wg, $url) {
+                Coroutine::defer(function() use($wg) {
+                    $wg->done();
+                });
+                $data = $this->getData($url);
+                echo $url, " -------- ", md5($data), "\n";
+            });
+        }
+        $wg->wait();
+    }
+
+    public function getData($url) {
+        $data = "";
+        try {
+            $client = $this->clientFactory->create([]);
+            $response = $client->request("GET", $url);
+            $body = $response->getBody();
+            while (!$body->eof()) {
+                $data .= $body->read(1024);
+            }
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+        return $data;
     }
 }
